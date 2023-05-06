be91c86706dc01b41a0c6bb1aaf5b9b077fd1a3f
Herbie <hjrand1005@gmail.com>
2023-05-06 09:50:10 -0400

    ADD: 2-2

---
+++ c-book/ch2/2-2/2-2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXLINE 100
+
+int get_line(char[], int);
+
+int main() {
+    char line[MAXLINE];
+
+    while (get_line(line, MAXLINE))
+        printf("line: %s", line);
+}
+
+int get_line(char s[], int lim) {
+    int i, c;
+
+    i = 0;
+    while (i<lim-1) {
+        if ((c=getchar()) == '\n')
+            break;
+        if (c == EOF)
+            break;
+        s[i] = c;
+        i++;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
+
+    return i;
+}
