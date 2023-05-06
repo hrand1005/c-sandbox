55a2b429bb87cf7574db894a8e2b61a81c0415e1
Herbie <hjrand1005@gmail.com>
2023-05-06 16:39:17 -0400

    ADD: 2-4, 2-5

---
+++ c-book/ch2/2-4/2-4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void print_squeeze(char[], char[]);
+void squeeze(char[], char[]);
+
+int main() {
+    char word1[] = "systems";
+    char sub1[] = "st";
+
+    char word2[] = "systems";
+    char sub2[] = "ym";
+
+    char word3[] = "applications";
+    char sub3[] = "al";
+
+    char word4[] = "applications";
+    char sub4[] = "is";
+
+    char word5[] = "systems, applications";
+    char sub5[] = "qz";
+
+    print_squeeze(word1, sub1);
+    print_squeeze(word2, sub2);
+    print_squeeze(word3, sub3);
+    print_squeeze(word4, sub4);
+    print_squeeze(word5, sub5);
+}
+
+void print_squeeze(char word[], char sub[]) {
+    printf("squeeze(\"%s\", \"%s\"): ", word, sub);
+    squeeze(word, sub);
+    printf("\"%s\"\n\n", word);
+}
+
+void squeeze(char word[], char sub[]) {
+    int i, j, k, contains;
+
+    for (i = j = 0; word[i] != '\0'; i++) {
+        for (contains = k = 0; sub[k] != '\0' && !contains; k++)
+            contains = (word[i] == sub[k]);
+        if (!contains) {
+            word[j++] = word[i];
+        }
+    }
+    word[j] = '\0';
+}
---
+++ c-book/ch2/2-5/2-5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void print_any(char[], char[]);
+int any(char[], char[]);
+
+int main() {
+    char word1[] = "systems";
+    char sub1[] = "st";
+
+    char word2[] = "systems";
+    char sub2[] = "ym";
+
+    char word3[] = "applications";
+    char sub3[] = "al";
+
+    char word4[] = "applications";
+    char sub4[] = "is";
+
+    char word5[] = "systems, applications";
+    char sub5[] = "qz";
+
+    print_any(word1, sub1);
+    print_any(word2, sub2);
+    print_any(word3, sub3);
+    print_any(word4, sub4);
+    print_any(word5, sub5);
+}
+
+void print_any(char word[], char sub[]) {
+    printf("any(\"%s\", \"%s\"): ", word, sub);
+    printf("%d\n\n", any(word, sub));
+}
+
+int any(char word[], char sub[]) {
+    int i, j;
+
+    for (i = 0; word[i] != '\0'; i++) {
+        for (j = 0; sub[j] != '\0'; j++)
+            if (word[i] == sub[j])
+                return i;
+    }
+
+    return -1;
+}
